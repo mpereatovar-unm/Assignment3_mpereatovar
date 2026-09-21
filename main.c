38362450dc27346f42fb556a21f0f4d7ab3201d4
Monica Perea <mpereatovar@unm.edu>
2026-09-21 12:43:29 -0600

    new commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // printf
+#include <stdlib.h> // malloc, free , atoi (ASCII to integer)
+#include "array.h" // Array Structure
+
+int main( int argc, char **argv)
+{ 
+    Array *a1; //created a pointer to an Array structure with *a1
+
+    return 0;
+
+}
+
+
