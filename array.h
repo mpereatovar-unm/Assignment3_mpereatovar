5825789057868c18eb23d2a21bc2af84966068ca
Monica Perea <mpereatovar@unm.edu>
2026-09-21 11:54:06 -0600

    first test commit VSCode

---
+++ array.h
@@ -0,0 +1,13 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+// Array implementation details
+struct _my_array
+{
+    int size;
+    double *data;
+};
+typedef struct _my_array ARRAY;
+
+#endif // ARRAY_H
+
